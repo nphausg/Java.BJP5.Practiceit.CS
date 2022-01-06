a56a88a6700ad5c1000cf8a975b283e863340a90
nphau <tieuhauvn@gmail.com>
2022-01-07 00:13:11 +0700

    feat: add calendarDate

---
+++ Chapter10_ArrayList/CalendarDate.java
@@ -0,0 +1,61 @@
+/*
+ * Created by nphau on 06/01/2022, 09:50
+ * Copyright (c) 2022. All rights reserved.
+ * Last modified 06/01/2022, 09:50
+ */
+
+package Chapter10_ArrayList;
+/**
+    Suppose that a class CalendarDate has been defined for storing a calendar date with month, day and year components.
+    (In our section handout, the class was called Date, but we have renamed it here because Practice-It uses a class named Date for other purposes.) The class includes the following members:
+
+    Name	Description
+    private int year	the year component
+    private int month	the month component
+    private int day	the day component
+    public CalendarDate(int year, int month, int day)	constructs a date with given year, month, day
+    public int getYear()	returns the year component
+    public int getMonth()	returns the month component
+    public int getDay()	returns the day component
+    public String toString()	returns the date in yyyy/mm/dd format
+    Your task is to modify the class to be Comparable by adding an appropriate compareTo method. Dates that occur chronologically 
+    earlier should be considered "less" than dates that occur later.
+
+    You may assume that dates are constructed with appropriate values: Months will be between 1 and 12, 
+    days will be between 1 and 31 and years will be four-digit numbers. Assume that the toString method produces a standard format 
+    with two digits for the month and two digits for the day, including leading zeros if necessary. For example, January 7th, 2005, would return the string "2005/01/07".
+ */
+public class CalendarDate {
+
+    private int year;   //	the year component
+    private int month;  //  the month component
+    private int day;    //	the day component
+    // 	constructs a date with given year, month, day
+    public CalendarDate(int year, int month, int day){
+
+    }
+    public int getYear(){
+        return year;
+    }
+    public int getMonth(){
+        return month;
+    }
+    public int getDay(){
+        return day;
+    }
+    
+    // https://practiceit.cs.washington.edu/problem/view/bjp5/chapter10/e21-ComparableCalendarDate
+    public int compareTo(CalendarDate date) {
+        if (year == date.year && month == date.month && day == date.day) {
+            return 0;
+        } else if (year == date.year && month == date.month && day > date.day) {
+            return 1;
+        } else if (year == date.year && month > date.month) {
+            return 1;
+        } else if (year > date.year) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# -Java.BJP5.Practiceit.CS
-Java online programming problems from Practice, provided by the University of Washington https://practiceit.cs.washington.edu/problem/list
+# Java.BJP5.Practiceit.CS
+Practice-it is a web application to help you practice solving Java programming problems online. Many of the problems come from the University of Washington's introductory Java courses.
+
+To use Practice-it, first create an account, then choose a problem from our list. Type a solution and submit it to our server. The system will test it and tell you whether your solution is correct. https://practiceit.cs.washington.edu/problem/list
+
+[![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)](#table-of-contents)
+
+## 👇 Authors
+<p>
+    <a href="https://nphau.medium.com/" target="_blank">
+    <img src="https://avatars2.githubusercontent.com/u/13111806?s=400&u=f09b6160dbbe2b7eeae0aeb0ab4efac0caad57d7&v=4" width="96" height="96">
+    </a>
+</p>
